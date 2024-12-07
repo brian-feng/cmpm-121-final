2f86d36027920929dca95f1cd4a3fc4bf19fab8e
ryviance <eion.ling0@gmail.com>
2024-12-07 12:17:56 -0800

    Refactor board.ts, plant.ts, player.ts

--- src/board.ts
+++ src/board.ts
@@ -3,10 +3,11 @@ import "./plant.ts";
 import Position from "./position.ts";
 import BoardTile from "./boardTile.ts";
 import { getPlantColorByIndex, getXPIncreaseByIndex } from "./plant.ts";
-import { type GameSettings, initGameSettings } from "./externalDSLParser.ts";
+import { type GameSettings } from "./externalDSLParser.ts";
 
 const SPACEWIDTH: number = 50;
 const SPACEHEIGHT: number = 50;
+
 export default class Board {
   width: number = SPACEWIDTH;
   height: number = SPACEHEIGHT;
@@ -14,7 +15,7 @@ export default class Board {
   ctx: CanvasRenderingContext2D;
   playerPos: Position;
   gameSettings: GameSettings | null;
-  // amtHarvested: number;
+
   constructor(
     ctx: CanvasRenderingContext2D,
     tiles: BoardTile[],
@@ -23,150 +24,95 @@ export default class Board {
     this.gameSettings = gameSettings;
     this.tiles = tiles;
     this.ctx = ctx;
-    this.playerPos = { x: 0, y: 0 }; // render default position if no playerTile
-    const playerTile = tiles.find((tile) => tile.hasPlayer == true);
-    if (playerTile) {
-      this.playerPos = { x: playerTile.xPos, y: playerTile.yPos };
-    }
-    // if (amtHarvested) {
-    //   this.amtHarvested = amtHarvested;
-    // } else {
-    //   this.amtHarvested = 0;
-    // }
+    this.playerPos = this.findPlayerPosition(tiles);
     this.drawPlayer(ctx);
   }
 
+  // Helper function to find the player’s initial position
+  private findPlayerPosition(tiles: BoardTile[]): Position {
+    const playerTile = tiles.find((tile) => tile.hasPlayer);
+    return playerTile ? { x: playerTile.xPos, y: playerTile.yPos } : { x: 0, y: 0 };
+  }
+
+  // Adds a tile to the board and renders it
   addTile(tile: BoardTile) {
     this.tiles.push(tile);
-    if (tile.tileColor == 1) {
-      this.ctx!.fillStyle = this.gameSettings!.board_info.board_colors[0]!;
-    } else this.ctx!.fillStyle = this.gameSettings!.board_info.board_colors[1]!;
-    this.ctx!.fillRect(tile.xPos, tile.yPos, tile.width, tile.height);
+    this.renderTile(tile);
+  }
 
+  // Draws a single tile on the board
+  drawTile(ctx: CanvasRenderingContext2D, tile: BoardTile) {
+    this.renderTile(tile);
+  }
+
+  private renderTile(tile: BoardTile) {
+    const color = this.getTileColor(tile.tileColor);
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(tile.xPos, tile.yPos, tile.width, tile.height);
     if (tile.cropLevel > 0) this.drawPlant(this.ctx, tile);
   }
 
-  drawTile(ctx: CanvasRenderingContext2D, tile: BoardTile) {
-    if (tile.tileColor == 1) {
-      ctx.fillStyle = this.gameSettings!.board_info.board_colors[0]!;
-    } else ctx.fillStyle = this.gameSettings!.board_info.board_colors[1]!;
-
-    ctx.fillRect(tile.xPos, tile.yPos, tile.width, tile.height);
-
-    if (tile.cropLevel > 0) this.drawPlant(ctx, tile);
-  }
-
+  // Advances time by updating tiles' crop levels and other properties
   advanceTime(ctx: CanvasRenderingContext2D) {
     this.tiles.forEach((tile) => {
-      // There is an advanceTime function for each tile
-      if (tile.cropLevel < 3 && tile.cropLevel > 0) {
-        //!Sunlight and water level might need tweaking, confusing when plants dont grow properly
-        if (tile.waterLevel > 0 && tile.sunlightLevel > 0) {
-          if (tile.plantID != 0) {
-            tile.plantXP += getXPIncreaseByIndex(tile.plantID, this, tile);
-          }
-        }
-        if (tile.plantXP >= 10) {
-          tile.cropLevel += 1;
-          tile.plantXP = 0;
-        }
+      if (tile.cropLevel > 0 && tile.cropLevel < 3) {
+        this.growPlant(tile);
       }
-      // Adjust thte sunlight and water levels
       tile.waterLevel = this.getAdjacentWaters(tile);
       tile.sunlightLevel = generateRandomInt(0, 3);
-      // Redraw only the plant without resetting the space
       this.refreshSpace(ctx, tile);
     });
   }
 
-  getLevel3Plants() {
-    //Get count of level 3 plants and return as int
-    let count: number = 0;
-    console.log(this.tiles);
-    this.tiles.forEach((tile) => {
-      if (tile.cropLevel == 3) {
-        count++;
+  // Grows the plant by checking its water and sunlight levels and updating its crop level
+  private growPlant(tile: BoardTile) {
+    if (tile.waterLevel > 0 && tile.sunlightLevel > 0) {
+      if (tile.plantID !== 0) {
+        tile.plantXP += getXPIncreaseByIndex(tile.plantID, this, tile);
       }
-    });
-    console.log(count);
-    return count;
+      if (tile.plantXP >= 10) {
+        tile.cropLevel += 1;
+        tile.plantXP = 0;
+      }
+    }
   }
 
+  // Counts the number of level 3 plants
+  getLevel3Plants(): number {
+    return this.tiles.filter(tile => tile.cropLevel === 3).length;
+  }
+
+  // Gets adjacent tiles to the given tile
   getAdjacentTiles(tile: BoardTile): BoardTile[] {
-    let adjTiles: BoardTile[] = [];
-    const tempX = tile.xPos;
-    const tempY = tile.yPos;
-    if (tempX > 1) {
-      const tempTile = this.getSpace({ x: tempX - 51, y: tempY });
-      if (tempTile) {
-        adjTiles.push(tempTile);
-      }
-    }
-    if (tempX < 24 * 51) {
-      const tempTile = this.getSpace({ x: tempX + 51, y: tempY });
-      if (tempTile) {
-        adjTiles.push(tempTile);
-      }
-    }
-    if (tempY > 1) {
-      const tempTile = this.getSpace({ x: tempX, y: tempY - 51 });
-      if (tempTile) {
-        adjTiles.push(tempTile);
-      }
-    }
-    if (tempY < 13 * 51) {
-      const tempTile = this.getSpace({ x: tempX, y: tempY + 51 });
-      if (tempTile) {
-        adjTiles.push(tempTile);
-      }
-    }
+    const adjTiles: BoardTile[] = [];
+    const directions: Position[] = [
+      { x: -51, y: 0 },  // Left
+      { x: 51, y: 0 },   // Right
+      { x: 0, y: -51 },  // Up
+      { x: 0, y: 51 },   // Down
+    ];
+    
+    directions.forEach(({ x, y }) => {
+      const adjTile = this.getSpace({ x: tile.xPos + x, y: tile.yPos + y });
+      if (adjTile) adjTiles.push(adjTile);
+    });
+
     return adjTiles;
   }
 
-  // Might delete this function
-  getAdjacentWaters(tile: BoardTile) {
-    let adjWaters: number = 0;
-    const tempX = tile.xPos;
-    const tempY = tile.yPos;
-    // const tempX = this.position.x/51;
-    // const tempY = this.position.y/51;
-    if (tempX > 1) {
-      const tempTile = this.getSpace({ x: tempX - 51, y: tempY });
-      if (tempTile) {
-        adjWaters += tempTile.waterLevel;
-      }
-    }
-    if (tempX < 24) {
-      const tempTile = this.getSpace({ x: tempX + 51, y: tempY });
-      if (tempTile) {
-        adjWaters += tempTile.waterLevel;
-      }
-    }
-    if (tempY > 1) {
-      const tempTile = this.getSpace({ x: tempX, y: tempY - 51 });
-      if (tempTile) {
-        adjWaters += tempTile.waterLevel;
-      }
-    }
-    if (tempY < 13) {
-      const tempTile = this.getSpace({ x: tempX, y: tempY + 51 });
-      if (tempTile) {
-        adjWaters += tempTile.waterLevel;
-      }
-    }
-    return adjWaters / 1.7;
+  // Calculates the adjacent water levels
+  private getAdjacentWaters(tile: BoardTile): number {
+    const adjacentTiles = this.getAdjacentTiles(tile);
+    const totalWater = adjacentTiles.reduce((sum, adjTile) => sum + adjTile.waterLevel, 0);
+    return totalWater / 1.7;
   }
 
-  getSpace(pos: Position) {
-    const found = this.tiles.find((tile) =>
-      tile.xPos == pos.x && tile.yPos == pos.y
-    );
-    if (found) {
-      return found;
-    } else {
-      console.log("not found");
-    }
+  // Finds a tile at the given position
+  getSpace(pos: Position): BoardTile | undefined {
+    return this.tiles.find(tile => tile.xPos === pos.x && tile.yPos === pos.y);
   }
+
+  // Refreshes the space by redrawing the tile and its plant
   refreshSpace(ctx: CanvasRenderingContext2D, tile: BoardTile) {
     this.drawTile(ctx, tile);
     if (tile.cropLevel > 0) {
@@ -177,55 +123,54 @@ export default class Board {
     }
   }
 
+  // Moves the player by the specified x and y offsets
   playerMove(ctx: CanvasRenderingContext2D, x: number, y: number) {
-    //const prevPos = {currX, currY};
     const currPos = this.getSpace(this.playerPos);
     if (currPos) {
-      this.playerPos.x += x * 51;
-      this.playerPos.y += y * 51;
-
-      const newPos = this.getSpace(this.playerPos);
+      const newPos = this.getSpace({ x: this.playerPos.x + x * 51, y: this.playerPos.y + y * 51 });
       if (newPos) {
-        console.log("newPos: ", newPos);
+        this.playerPos.x += x * 51;
+        this.playerPos.y += y * 51;
         this.refreshSpace(ctx, currPos);
-        // this.playerPos = {x: newX, y: newY};
         this.drawPlayer(ctx);
         currPos.hasPlayer = false;
         newPos.hasPlayer = true;
-      } else {
-        this.playerPos.x -= x * 51;
-        this.playerPos.y -= y * 51;
       }
     }
   }
 
+  // Draws the player on the canvas
   drawPlayer(ctx: CanvasRenderingContext2D) {
     const currentTile = this.getSpace(this.playerPos);
-    if (currentTile) {
+    if (currentTile) { // Check if currentTile is defined
       currentTile.hasPlayer = true;
+      ctx.fillStyle = "black";
+      ctx.fillRect(this.playerPos.x, this.playerPos.y, this.width, this.height);
+  
+      if (currentTile.cropLevel > 0) { // Ensure cropLevel is accessed only if currentTile is defined
+        this.drawPlant(ctx, currentTile);
+      }
+    } else {
+      console.warn("Player's current tile not found!");
     }
-    ctx.fillStyle = "black";
-    ctx.fillRect(this.playerPos.x, this.playerPos.y, this.width, this.height);
+  }  
 
-    if (currentTile?.cropLevel! > 0) this.drawPlant(ctx, currentTile!);
-  }
-
+  // Harvests the plant on the current tile
   harvestHere(ctx: CanvasRenderingContext2D) {
     const currentTile = this.getSpace(this.playerPos);
-    if (currentTile) {
-      if (currentTile.cropLevel > 0) {
-        currentTile.plantID = 0;
-        currentTile.plantXP = 0;
-        currentTile.cropLevel = 0;
-        this.refreshSpace(ctx, currentTile);
-      }
+    if (currentTile && currentTile.cropLevel > 0) {
+      currentTile.plantID = 0;
+      currentTile.plantXP = 0;
+      currentTile.cropLevel = 0;
+      this.refreshSpace(ctx, currentTile);
     }
   }
 
+  // Places a plant on the current tile or harvests it if already planted
   placeHere(ctx: CanvasRenderingContext2D, plantID: number) {
     const currentTile = this.getSpace(this.playerPos);
     if (currentTile) {
-      if (currentTile.plantID == 0) {
+      if (currentTile.plantID === 0) {
         currentTile.plantID = plantID;
         currentTile.cropLevel = 1;
         this.drawPlant(ctx, currentTile);
@@ -235,30 +180,41 @@ export default class Board {
     }
   }
 
-  drawPlant(ctx: CanvasRenderingContext2D, tile: BoardTile) {
-    if (tile.plantID != 0) ctx.fillStyle = getPlantColorByIndex(tile.plantID);
+  // Draws the plant on the tile based on its level
+  public drawPlant(ctx: CanvasRenderingContext2D, tile: BoardTile) {
+    if (tile.plantID !== 0) {
+      ctx.fillStyle = getPlantColorByIndex(tile.plantID);
+      const sizePercentage = this.getPlantSizeByLevel(tile.cropLevel);
+      const newWidth = tile.width * sizePercentage;
+      const newHeight = tile.height * sizePercentage;
 
-    let sizePercentage = 0;
-    if (tile.cropLevel == 1) {
-      sizePercentage = 0.4;
-    } else if (tile.cropLevel == 2) {
-      sizePercentage = 0.6;
-    } else {
-      sizePercentage = 0.8;
+      ctx.fillRect(
+        tile.xPos + (tile.width - newWidth) / 2,
+        tile.yPos + (tile.height - newHeight) / 2,
+        newWidth,
+        newHeight
+      );
     }
-
-    const newWidth = tile.width * sizePercentage;
-    const newHeight = tile.height * sizePercentage;
-
-    ctx.fillRect(
-      tile.xPos + (tile.width - newWidth) / 2,
-      tile.yPos + (tile.height - newHeight) / 2,
-      newWidth,
-      newHeight,
-    );
   }
 
+  // Returns the plant size percentage based on its level
+  private getPlantSizeByLevel(level: number): number {
+    switch (level) {
+      case 1: return 0.4;
+      case 2: return 0.6;
+      case 3: return 0.8;
+      default: return 0.4;
+    }
+  }
+
+  // Sets the board tiles
   setTiles(tiles: BoardTile[]) {
     this.tiles = tiles;
+  }
+
+  // Retrieves the color for a given tile
+  private getTileColor(tileColor: number): string {
+    const colorIndex = tileColor === 1 ? 0 : 1;
+    return this.gameSettings!.board_info.board_colors[colorIndex]!;
   }
 }
--- src/plant.ts
+++ src/plant.ts
@@ -1,14 +1,6 @@
 import Position from "./position.ts";
 import Board from "./board.ts";
 import BoardTile from "./boardTile.ts";
-// export default class PlantType {
-// future plans
-// to select plant type
-// the other plant will be normal class
-// this is so that you can instantiate species with one class
-// in the other class, the boardspace will instantiate it and
-// automatically plant it on instantiation
-// }
 
 interface PlantType {
   index: number;
@@ -17,37 +9,36 @@ interface PlantType {
   gainXP: (board: Board, tileParam: BoardTile) => number;
 }
 
-function increaseXPForEachNonPlantTile(Board: Board, tileParam: BoardTile) {
+// General function for XP calculation based on adjacent tiles and conditions
+function calculateXPForAdjacentTiles(
+  Board: Board,
+  tileParam: BoardTile,
+  condition: (tile: BoardTile) => boolean,
+  xpIncrement: number
+): number {
   let tempXP = 0;
   Board.getAdjacentTiles(tileParam).forEach((tile) => {
-    if (tile.cropLevel == 0) {
-      tempXP += 2;
+    if (condition(tile)) {
+      tempXP += xpIncrement;
     }
   });
   return tempXP;
 }
+
+// Specific XP gain functions
+function increaseXPForEachNonPlantTile(Board: Board, tileParam: BoardTile) {
+  return calculateXPForAdjacentTiles(Board, tileParam, (tile) => tile.cropLevel === 0, 2);
+}
+
 function increaseXPForAdjacentPlants(Board: Board, tileParam: BoardTile) {
-  let tempXP = 0;
-  console.log("Getting Adjacent Tiles");
-  Board.getAdjacentTiles(tileParam).forEach((tile) => {
-    console.log("Checking tile", tile);
-    if (tile.cropLevel > 0) {
-      tempXP += 1;
-    }
-  });
-  return tempXP;
+  return calculateXPForAdjacentTiles(Board, tileParam, (tile) => tile.cropLevel > 0, 1);
 }
 
 function increaseXPForAdjacentSamePlants(Board: Board, tileParam: BoardTile) {
-  let tempXP = 0;
-  Board.getAdjacentTiles(tileParam).forEach((tile) => {
-    if (tile.cropLevel > 0 && tile.plantID == tileParam.plantID) {
-      tempXP += 2;
-    }
-  });
-  return tempXP;
+  return calculateXPForAdjacentTiles(Board, tileParam, (tile) => tile.cropLevel > 0 && tile.plantID === tileParam.plantID, 2);
 }
 
+// Simplify plant definitions with generalized logic
 const plants: PlantType[] = [
   {
     index: 1,
@@ -63,7 +54,12 @@ const plants: PlantType[] = [
     gainXP: (board: Board, tileParam: BoardTile) =>
       increaseXPForAdjacentSamePlants(board, tileParam),
   },
-  { index: 3, color: "brown", name: "Brown", gainXP: () => 10 },
+  {
+    index: 3,
+    color: "brown",
+    name: "Brown",
+    gainXP: () => 10, // Fixed XP gain
+  },
   {
     index: 4,
     color: "red",
@@ -73,6 +69,7 @@ const plants: PlantType[] = [
   },
 ];
 
+// Utility functions to get plant data by index
 export function getPlantsArray(): PlantType[] {
   return plants;
 }
@@ -97,54 +94,4 @@ export function getXPIncreaseByIndex(
   const XPAmount = plants[index - 1].gainXP(board, tile);
   console.log(XPAmount);
   return XPAmount;
-}
-
-//Might be useless now, but not sure if its ready to delete
-
-export default class Plant {
-  color: number;
-  name: number;
-  constructor(color: number, name: number) {
-    this.color = color;
-    this.name = name;
-  }
-  placePlant() {
-  }
-  draw(
-    ctx: CanvasRenderingContext2D,
-    pos: Position,
-    w: number,
-    h: number,
-    level: number,
-  ) {
-    ctx.fillStyle = this.color.toString();
-
-    // Calculate the plant size based on the level
-    let sizePercentage = 0;
-    switch (level) {
-      case 1:
-        sizePercentage = 0.4; // 40%
-        break;
-      case 2:
-        sizePercentage = 0.6; // 60%
-        break;
-      case 3:
-        sizePercentage = 0.8; // 80%
-        break;
-      default:
-        sizePercentage = 0.4; // Default to level 1 if the level is invalid
-    }
-
-    // Width and height based on %
-    const newWidth = w * sizePercentage;
-    const newHeight = h * sizePercentage;
-
-    // Center the plant within the space
-    ctx.fillRect(
-      pos.x + (w - newWidth) / 2, // Horizontal center alignment
-      pos.y + (h - newHeight) / 2, // Vertical center alignment
-      newWidth,
-      newHeight,
-    );
-  }
 }
